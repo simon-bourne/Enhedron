64c80e239690f6fe98aaf2b8671efe3a041792cd
Simon Bourne <simonbourne@enhedron.com>
2015-12-05 15:40:19 +0000

    Refactored results writers

--- cpp/test/include/Enhedron/Test/Results.h
+++ cpp/test/include/Enhedron/Test/Results.h
@@ -14,6 +14,7 @@
 #include <string>
 #include <stdexcept>
 #include <vector>
+#include <algorithm>
 
 namespace Enhedron { namespace Test { namespace Impl { namespace Impl_Results {
     using std::exception;
@@ -24,6 +25,7 @@ namespace Enhedron { namespace Test { namespace Impl { namespace Impl_Results {
     using std::endl;
     using std::vector;
     using std::move;
+    using std::max;
 
     using Assertion::FailureHandler;
     using Assertion::Variable;
@@ -115,9 +117,45 @@ namespace Enhedron { namespace Test { namespace Impl { namespace Impl_Results {
 
     class HumanResults final: public Results {
         Out<ostream> output_;
-        size_t depth_ = 0;
         Verbosity verbosity_;
         bool contextWritten_ = false;
+        size_t whenDepth_ = 0;
+
+        void writeContext(const NameStack& contextStack) {
+            if (verbosity_ >= Verbosity::CONTEXTS && ! contextWritten_) {
+                if ( ! contextStack.stack().empty()) {
+                    *output_ << contextStack.stack().front();
+
+                    for (
+                            auto contextIter = contextStack.stack().begin() + 1;
+                            contextIter != contextStack.stack().end();
+                            ++contextIter
+                        )
+                    {
+                        *output_ << "/" << *contextIter;
+                    }
+
+                    *output_ << "\n";
+                }
+
+                contextWritten_ = true;
+            }
+        }
+
+        void printVariables(const vector <Variable> &variableList) {
+            for (const auto& variable : variableList) {
+                indent(whenDepth_ + 1);
+                (*output_) << variable.name() << " = " << variable.value()
+                << ": file \"" << variable.file() << "\", line " << variable.line() << ".\n";
+            }
+        }
+
+        void indent(size_t indent) {
+            while (indent > 0) {
+                *output_ << "    ";
+                --indent;
+            }
+        }
     public:
         HumanResults(Out<ostream> output, Verbosity verbosity) :
             output_(output), verbosity_(verbosity)
@@ -126,107 +164,72 @@ namespace Enhedron { namespace Test { namespace Impl { namespace Impl_Results {
         virtual void finish(const Stats& stats) override {}
 
         virtual void beginContext(const NameStack& contextStack, const string& name) override {
-            if (verbosity_ >= Verbosity::CONTEXTS) {
-                if ( ! contextStack.stack().empty()) {
-                    for (const auto& parentName : contextStack.stack()) {
-                        *output_ << parentName << "/";
-                    }
-
-                }
-
-                *output_ << name << "\n";
-                contextWritten_ = true;
-            }
-            else {
-                contextWritten_ = false;
-            }
-        }
-
-        virtual void endContext(const Stats& stats, const NameStack& context, const string& name) override {
-            // TODO: Write context here.
             contextWritten_ = false;
         }
 
-        virtual void beginGiven(const NameStack& context, const string& given) {}
-        virtual void endGiven(const Stats& stats, const NameStack& context, const string& given) {}
+        virtual void endContext(const Stats& stats, const NameStack& context, const string& name) override {
+            contextWritten_ = false;
+        }
+
+        virtual void beginGiven(const NameStack& context, const string& given) override {
+            if (verbosity_ >= Verbosity::CONTEXTS) {
+                writeContext(context);
+            }
+
+            if (verbosity_ >= Verbosity::FIXTURES) {
+                indent(1);
+                *output_ << "Given: " << given << "\n";
+            }
+        }
+
+        virtual void endGiven(const Stats& stats, const NameStack& context, const string& given) override { }
 
         virtual void beginWhen(const NameStack& context,
                                const string& given,
                                const NameStack& whenStack,
-                               const string& when) override {}
+                               const string& when) override {
+            ++whenDepth_;
+
+            if (verbosity_ >= Verbosity::SECTIONS) {
+                indent(whenDepth_);
+                *output_ << " When: " << when << "\n";
+            }
+        }
+
         virtual void endWhen(const Stats& stats,
                              const NameStack& context,
                              const string& given,
                              const NameStack& whenStack,
-                             const string& when) override {}
+                             const string& when) override {
+            --whenDepth_;
 
-        virtual void failByException(const exception& e) override {}
+            if (whenDepth_ == 0 && verbosity_ >= Verbosity::CHECKS) {
+                *output_ << "\n";
+            }
+        }
 
         virtual bool notifyPassing() const override { return verbosity_ >= Verbosity::CHECKS; }
 
         virtual void fail(optional<string> description, const string &expressionText, const vector <Variable> &variableList) override {
-        }
-
-        virtual void pass(optional<string> description, const string &expressionText, const vector <Variable> &variableList) override {
-
-        }
-    };
-/*
-    class HumanResultTest final: public ResultTest {
-        Out<ostream> outputStream_;
-        size_t depth_;
-        Verbosity verbosity_;
-
-        void indent(size_t relativeDepth) {
-            for (size_t totalDepth = 1 + depth_ + relativeDepth; totalDepth > 0; --totalDepth) {
-                (*outputStream_) << "    ";
-            }
-        }
-
-        void printVariables(const vector <Variable> &variableList) {
-            for (const auto& variable : variableList) {
-                indent(2);
-                (*outputStream_) << variable.name() << " = " << variable.value()
-                << ": file \"" << variable.file() << "\", line " << variable.line() << ".\n";
-            }
-        }
-    public:
-        HumanResultTest(Out<ostream> outputStream, size_t depth, Verbosity verbosity) :
-                outputStream_(outputStream), depth_(depth), verbosity_(verbosity)
-        {}
-
-        virtual unique_ptr<ResultTest> section(string description) override {
-            if (verbosity_ >= Verbosity::SECTIONS) {
-                indent(1);
-                (*outputStream_) << "when: " << description << "\n";
-            }
-
-            return make_unique<HumanResultTest>(outputStream_, depth_ + 1, verbosity_);
-        }
-
-        virtual void beforeFirstFailure() override {}
-
-        virtual bool notifyPassing() const override { return verbosity_ >= Verbosity::CHECKS; }
-
-        virtual void fail(optional<string> description, const string &expressionText, const vector <Variable> &variableList) override {
-            indent(1);
-            (*outputStream_) << "CHECK FAILED: " << expressionText << "\n";
+            indent(whenDepth_);
+            (*output_) << "CHECK FAILED: " << expressionText << "\n";
             printVariables(variableList);
         }
 
         virtual void pass(optional<string> description, const string &expressionText, const vector <Variable> &variableList) override {
-            indent(1);
-            (*outputStream_) << "then";
+            static constexpr const size_t minDepth = 1;
+            indent(max(minDepth, whenDepth_));
+            (*output_) << " Then: ";
 
             if (description) {
-                (*outputStream_) << ": " << *description;
+                (*output_) << *description;
             }
 
-            if (verbosity_ >= Verbosity::CHECKS_EXPRESSION) {
-                (*outputStream_) << ": " << expressionText;
+            if (verbosity_ >= Verbosity::CHECKS_EXPRESSION || ! description) {
+                (*output_) << expressionText;
             }
 
-            (*outputStream_) << "\n";
+            (*output_) << "\n";
 
             if (verbosity_ >= Verbosity::VARIABLES) {
                 printVariables(variableList);
@@ -234,91 +237,10 @@ namespace Enhedron { namespace Test { namespace Impl { namespace Impl_Results {
         }
 
         virtual void failByException(const exception& e) override {
-            indent(0);
-            (*outputStream_) << "TEST FAILED WITH EXCEPTION: " << e.what() << endl;
-        }
-
-        virtual void finish(const Stats& stats) override {}
-
-    };
-
-    class HumanResultContext final: public ResultContext {
-        Out <ostream> outputStream_;
-        Verbosity verbosity_;
-        string path_;
-
-    public:
-        HumanResultContext(Out<ostream> outputStream, Verbosity verbosity, const string& name) :
-                outputStream_(outputStream), verbosity_(verbosity), path_(name) {}
-
-        virtual void beforeFirstTestRuns() override {
-            if (verbosity_ >= Verbosity::CONTEXTS) {
-                *outputStream_ << path_ << "\n";
-            }
-        }
-
-        virtual void beforeFirstFailure() override { }
-
-        virtual unique_ptr<ResultContext> child(const string& name) override {
-            string childPath(path_);
-
-            if ( ! path_.empty()) {
-                childPath += "/";
-            }
-
-            childPath += name;
-
-            return make_unique<HumanResultContext>(outputStream_, verbosity_, childPath);
-        }
-
-        virtual unique_ptr<ResultTest> test(const string& name) override {
-            if (verbosity_ >= Verbosity::FIXTURES) {
-                *outputStream_ << "    Given: " << name << endl;
-            }
-
-            return make_unique<HumanResultTest>(outputStream_, 0, verbosity_);
-        }
-
-        virtual void finish(const Stats& stats) override {}
-    };
-
-    class HumanResultRootContext final: public ResultContext {
-        Out <ostream> outputStream_;
-        Verbosity verbosity_;
-    public:
-        HumanResultRootContext(Out<ostream> outputStream, Verbosity verbosity) :
-            outputStream_(outputStream), verbosity_(verbosity) {}
-
-        virtual void beforeFirstTestRuns() override { }
-
-        virtual void beforeFirstFailure() override { }
-
-        virtual unique_ptr<ResultContext> child(const string& name) override {
-            return make_unique<HumanResultContext>(outputStream_, verbosity_, name);
-        }
-
-        virtual unique_ptr<ResultTest> test(const string& name) override {
-            return make_unique<HumanResultTest>(outputStream_, 0, verbosity_);
-        }
-
-        virtual void finish(const Stats& stats) override {
-            if (verbosity_ >= Verbosity::SUMMARY) {
-                if (stats.failedTests() > 0) {
-                    *outputStream_ << "FAILED TESTS: " << stats.failedTests() << "\n";
-                }
-
-                if (stats.failedChecks() > 0) {
-                    *outputStream_ << "FAILED CHECKS: " << stats.failedChecks() << "\n";
-                }
-
-                *outputStream_ << "Totals: " <<
-                stats.tests() << " tests, " <<
-                stats.checks() << " checks, " <<
-                stats.fixtures() << " fixtures\n";
-            }
+            indent(whenDepth_);
+            (*output_) << "TEST FAILED WITH EXCEPTION: " << e.what() << endl;
         }
     };
-    */
 }}}}
 
 namespace Enhedron { namespace Test {
